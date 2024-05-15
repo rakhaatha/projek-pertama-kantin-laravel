b361174bbf645da9a2895e09b22a2921380f9f9e
rakhaatha <rakha.athallah.fwz@gmail.com>
2024-05-15 15:16:15 +0700

    tambahan di section1.php

--- app/Models/section1.php
+++ app/Models/section1.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use illuminate\Support\Facades\Storage;
 
 class section1 extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'thumbnail', 'content', 'post_as'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::updating(function($model){
+            if($model->isDirty('thumbnail') && ($model->getOriginal('thumbnail') !== null)) {
+                storage::disk('public')->delete($model->getOriginal('thumbnial'));
+}
+        });
+    }
 }
